56a8086c5e441d18ad855a75e3da9a96bf45f340
BBK-PiJ-2012-00 <ataylo20@dcs-bbk-ac-uk>
2013-01-26 14:30:14 +0000

    Complete (alternate way of sorting class Person using TreeSet constructor).

---
+++ PersonV2.java
@@ -0,0 +1,32 @@
+/**
+ * Q4
+ */
+package week2;
+
+/**
+ * @author Anna Taylor
+ *
+ */
+public class PersonV2 {
+	private String firstName;
+	private String lastName;
+	
+	public PersonV2(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	
+	public String toString() { //for printing meaningful details rather than memory address
+		return "Name: " + firstName + " " + lastName + "\n";
+	}
+
+}
