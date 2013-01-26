c9fcbdd422c2d138a05a8d0ff627e4cd154e56ec
BBK-PiJ-2012-00 <ataylo20@dcs-bbk-ac-uk>
2013-01-26 10:10:21 +0000

    Complete.

--- HashSetRunner.java
+++ HashSetRunner.java
@@ -1,19 +1,47 @@
 package week2;
 
-public class HashSetRunner {
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Iterator;
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class HashSetRunner {
+	private static Set<String> hashSet = new HashSet<String>();
+	
+	public static void main(String[] args) {		
+		
+		hashSet.add("aaa");
+		hashSet.add("bbb");
+		hashSet.add("ccc");
+		hashSet.add("ddd");
+		hashSet.add("eee");
+		hashSet.add("fff");
+		hashSet.add("ggg");
+		hashSet.add("hhh");
+		hashSet.add("iii"); 
+		hashSet.add("aaa");
+		hashSet.add("bbb");
+		hashSet.add("ccc");
+		hashSet.add("ddd");
+		hashSet.add("eee");
+		hashSet.add("fff");
+		hashSet.add("ggg");
+		hashSet.add("hhh");
+		hashSet.add("iii"); 
+		
+		Iterator<String> iterator = hashSet.iterator();
+		
+		for (String s : hashSet) {
+			System.out.println(s);
+		}
 
 	}
 
 }
 
+//No duplicates are added to sets, hence each string appears only once, and the order isn't important (and thus isn't retained).
+
 /**
-WriteaclasscalledHashedSetRunnerwithamainmethodthatinstantiatesaHashedSet<String>. Add the following strings to the linked list:
+WriteaclasscalledHashedSetRunnerwithamainmethodthatinstantiatesaHashedSet<String>. Add the following strings to the linked hashSet:
 aaa, bbb, ccc, ddd, eee, fff, ggg, hhh, iii, aaa, bbb, ccc, ddd, eee, fff, ggg, hhh, iii
 (for the comma replace with a newline)
 Notice that each string appears twice. After adding all the strings, use an enhanced for loop to walk sequentially through the hash set, 
